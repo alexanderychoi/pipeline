c3fff6962cb15edf337cc4bf7f698422d10bbab3
thomsonlab <thomsonlabucsf@gmail.com>
2015-08-20 13:20:03 -0700

    POLY A FOR SISI

--- drop_tac_polyA/drop_titan.py
+++ drop_tac_polyA/drop_titan.py
@@ -42,7 +42,7 @@ fastq_files.sort()
 ############################### PREPROCESSING ###############################
 print "\n"
 print "**********************************"
-print "**    Starting preprocessing    **"
+print "Starting preprocessing for SISI :P"
 print "**********************************"
 print "\n"
 
@@ -95,7 +95,8 @@ for f1, f2 in grouped(fastq_files, 2):
 			else:
 				total_reads+=1
 			index_a =  [m.start() for m in re.finditer('(?='+error_str_a+')', f2_line2)]
-			f2_line2=f2_line2[:index_a[0]+1]
+			if not index_a:
+				f2_line2=f2_line2[:index_a[0]+1]
 			if f1_line2[:3] == 'TAC' and f1_line2[:6] != 'TACGGG':
 				if tso not in f2_line2:
 					barcode = f1_line2[tac_length:tac_length+barcode_length]
