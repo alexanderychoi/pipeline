c7118a6433fab9d97e3ac9af67a2185f1b83e288
gheimberg <gheimberg@gmail.com>
2015-10-12 09:51:50 -0700

    saving server params files

--- drop_tac_kmeans/drop_titan_params.py
+++ drop_tac_kmeans/drop_titan_params.py
@@ -19,32 +19,38 @@
 ####Path to .fastq files
 #dir_path_fastqs = '../../../RNAseq_files/DS2/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/drop_test_files/fastqs/'
-dir_path_fastqs = '/home/graham/dropseq_pipe/targeted_test_files/fastqs/'
+#dir_path_fastqs = '/home/graham/dropseq_pipe/targeted_test_files/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/miseq_test/fastqs/'
 #dir_path_fastqs = '../../data/DS2/fastqs/'
 #dir_path_fastqs = '../../data/drop_test/fastqs/'
 #dir_path_fastqs = '../../data/fastqs_titan/fastqs/'
 #dir_path_fastqs = '../../data/fastq_micro/fastqs/'
+#dir_path_fastqs = '/home/david/RNAseq_files/DS4-H2/fastqs/'
+dir_path_fastqs = '/home/david/RNAseq_files/DS8/fastqs/'
 
 ####Path to .sam files
 #dir_path_alignment = '../../../RNAseq_files/DS2/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/drop_test_files/alignment/'
-dir_path_alignment = '/home/graham/dropseq_pipe/targeted_test_files/alignment/'
+#dir_path_alignment = '/home/graham/dropseq_pipe/targeted_test_files/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/miseq_test/alignment/'
 #dir_path_alignment = '../data/DS2/alignment/'
 #dir_path_alignment = '../data/drop_test/alignment/'
 #dir_path_alignment = '../data/fastqs_titan/alignment/'
 #dir_path_alignment = '../data/fastq_micro/alignment/'
+#dir_path_alignment = '/home/david/RNAseq_files/DS4-H2/alignments/'
+dir_path_alignment = '/home/david/RNAseq_files/DS8/alignments/'
 
 ####Path to the reference genome files
 #reference_genome = '~/reference_genomes/mm9/Transcriptome/transcriptome'
-reference_genome = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcriptome'
+#reference_genome = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcriptome'
 #reference_genome = '../../reference_genomes/Homo_sapiens/UCSC/hg19/cds'
 #reference_genome = '../../reference_genomes/Human_Mouse/transcriptome'
+reference_genome = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcriptome'
 
 #### Path to the species fasta file
 #fasta_path = '../../reference_genomes/mm9/Transcriptome/transcripts.fa'
-fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcripts.fa'
+#fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcripts.fa'
+fasta_path = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcripts.fa'
 
 
 #######################################
@@ -72,7 +78,7 @@ barcode_length = 10
 tso = 'AAGCAGTGGTATCAACGCAGAGTAC'
 
 #### Number of cells expected (same as # of clusters)
-cell_num = 5
+cell_num = 20
 
 #### Occurence threshold:
 #occ_threshold = 5000
