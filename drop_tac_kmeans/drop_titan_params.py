c6872a4b1e78eb3de65e9187899e36fa36d97ae1
gheimberg <Graham.Heimberg@ucsf.edu>
2015-10-13 17:14:28 -0700

    remove TSO processing because it was too slow, may re-add later, but didnt seem to be too important at the moment anyway

--- drop_tac_kmeans/drop_titan_params.py
+++ drop_tac_kmeans/drop_titan_params.py
@@ -17,43 +17,32 @@
 #######################################
 
 ####Path to .fastq files
-#dir_path_fastqs = '../../../RNAseq_files/DS2/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/drop_test_files/fastqs/'
-dir_path_fastqs = '/home/graham/dropseq_pipe/pipeline/targeted_test_files/fastqs/'
+#dir_path_fastqs = '/home/graham/dropseq_pipe/pipeline/targeted_test_files/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/targeted_test_files/fastqs/'
+dir_path_fastqs = '/home/graham/dropseq_pipe/DS8/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/miseq_test/fastqs/'
-#dir_path_fastqs = '../../data/DS2/fastqs/'
-#dir_path_fastqs = '../../data/drop_test/fastqs/'
-#dir_path_fastqs = '../../data/fastqs_titan/fastqs/'
-#dir_path_fastqs = '../../data/fastq_micro/fastqs/'
 #dir_path_fastqs = '/home/david/RNAseq_files/DS4-H2/fastqs/'
-dir_path_fastqs = '/home/david/RNAseq_files/DS8/fastqs/'
+#dir_path_fastqs = '/home/david/RNAseq_files/DS8/fastqs/'
 
 ####Path to .sam files
-#dir_path_alignment = '../../../RNAseq_files/DS2/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/drop_test_files/alignment/'
-
-dir_path_alignment = '/home/graham/dropseq_pipe/pipeline/targeted_test_files/alignment/'
+#dir_path_alignment = '/home/graham/dropseq_pipe/pipeline/targeted_test_files/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/targeted_test_files/alignment/'
+dir_path_alignment = '/home/graham/dropseq_pipe/DS8/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/miseq_test/alignment/'
-#dir_path_alignment = '../data/DS2/alignment/'
-#dir_path_alignment = '../data/drop_test/alignment/'
-#dir_path_alignment = '../data/fastqs_titan/alignment/'
-#dir_path_alignment = '../data/fastq_micro/alignment/'
 #dir_path_alignment = '/home/david/RNAseq_files/DS4-H2/alignments/'
-dir_path_alignment = '/home/david/RNAseq_files/DS8/alignments/'
+#dir_path_alignment = '/home/david/RNAseq_files/DS8/alignments/'
 
 ####Path to the reference genome files
 #reference_genome = '~/reference_genomes/mm9/Transcriptome/transcriptome'
-#reference_genome = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcriptome'
-#reference_genome = '../../reference_genomes/Homo_sapiens/UCSC/hg19/cds'
-#reference_genome = '../../reference_genomes/Human_Mouse/transcriptome'
-reference_genome = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcriptome'
+reference_genome = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcriptome'
+
+#reference_genome = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcriptome'
 
 #### Path to the species fasta file
-#fasta_path = '../../reference_genomes/mm9/Transcriptome/transcripts.fa'
-#fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcripts.fa'
-fasta_path = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcripts.fa'
+fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcripts.fa'
+#fasta_path = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcripts.fa'
 
 
 #######################################
@@ -72,7 +61,7 @@ str_search='TAC'
 #str_search=''
 
 #### umi length:
-umi_length = 8
+umi_length = 9
 
 #### barcode length
 barcode_length = 10
@@ -103,6 +92,8 @@ processors = 8
 bowtie2_options = ['-D', '10', '-R', '1', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
 #very sensitive
 #bowtie2_options = ['-D', '20', '-R', '3', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
+
+
 
 ############################################################################################
 #######################         !! DONT TOUCH !!        ####################################
