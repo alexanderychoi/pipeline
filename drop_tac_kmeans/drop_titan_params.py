7511d51b87f5dca0a653268ffd5ade556f7c45d3
gheimberg <Graham.Heimberg@ucsf.edu>
2015-11-06 17:11:25 -0800

    changed away from single linkage, make sure distance threshold is chosen according to linkage method

--- drop_tac_kmeans/drop_titan_params.py
+++ drop_tac_kmeans/drop_titan_params.py
@@ -18,8 +18,10 @@
 
 ####Path to .fastq files
 #dir_path_fastqs = '/home/graham/dropseq_pipe/drop_test_files/fastqs/'
-dir_path_fastqs = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/fastqs/'
+#dir_path_fastqs = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/fastqs/'
+#dir_path_fastqs = '/home/graham/Sequencing/N704/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/DS9/fastqs/'
+dir_path_fastqs = '/home/graham/dropseq_pipe/DS13/250_diff_NSC/fastqs/'
 #dir_path_fastqs = '/home/graham/dropseq_pipe/miseq_test/fastqs/'
 #dir_path_fastqs = '/home/david/RNAseq_files/DS4-H2/fastqs/'
 #dir_path_fastqs = '/home/david/RNAseq_files/DS8/fastqs/'
@@ -27,9 +29,11 @@ dir_path_fastqs = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/fast
 
 ####Path to .sam files
 #dir_path_alignment = '/home/graham/dropseq_pipe/drop_test_files/alignment/'
-dir_path_alignment = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/alignment/'
+#dir_path_alignment = '/home/graham/Sequencing/N704/alignment/'
+#dir_path_alignment = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/DS9/alignment/'
 #dir_path_alignment = '/home/graham/dropseq_pipe/miseq_test/alignment/'
+dir_path_alignment = '/home/graham/dropseq_pipe/DS13/250_diff_NSC/alignment/'
 #dir_path_alignment = '/home/david/RNAseq_files/DS4-H2/alignments/'
 #dir_path_alignment = '/home/david/RNAseq_files/DS8/alignments/'
 #dir_path_alignment = '/home/david/TITAN_pipeline/pipeline_code/targeted_dropseq_test/alignment/'
@@ -37,10 +41,12 @@ dir_path_alignment = '/home/graham/dropseq_pipe/pipeline/targeted_dropseq_test/a
 ####Path to the reference genome files
 #reference_genome = '~/reference_genomes/mm9/Transcriptome/transcriptome'
 reference_genome = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcriptome'
+#reference_genome = '/home/graham/dropseq_pipe/reference_genomes/UCSC_hg19_mRNA/genome_index/mRNA'
 #reference_genome = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcriptome'
 
 #### Path to the species fasta file
 fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/transcripts.fa'
+#fasta_path = '/home/graham/dropseq_pipe/reference_genomes/UCSC_hg19_mRNA/seed_files/mRNA.fa'
 #fasta_path = '/home/david/TITAN_pipeline/reference_genomes/mm9/Transcriptome/transcripts.fa'
 
 
@@ -56,8 +62,8 @@ fasta_path = '/home/graham/dropseq_pipe/reference_genomes/mm9/Transcriptome/tran
 #######################
 
 #### String to search at beginning of R1:
-str_search='TAC'
-#str_search=''
+#str_search='TAC'
+str_search=''
 
 #### umi length:
 umi_length = 8
@@ -69,7 +75,7 @@ barcode_length = 12
 tso = 'AAGCAGTGGTATCAACGCAGAGTAC'
 
 #### Number of cells expected (same as # of clusters)
-cell_num = 6
+cell_num = 250
 
 
 #### threshold for filtering consecutive A's
@@ -88,9 +94,9 @@ bowtie2_dir=''
 #Server processors
 processors = 8
 #Less sensitive 
-bowtie2_options = ['-D', '10', '-R', '1', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
+#bowtie2_options = ['-D', '10', '-R', '1', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
 #very sensitive
-#bowtie2_options = ['-D', '20', '-R', '3', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
+bowtie2_options = ['-D', '20', '-R', '3', '-N', '0', '-L', '20','-i', 'S,1,0.50', '-p', str(processors), '--reorder', '--no-hd', '--met', '20']
 
 
 
